1929faaaed313f99ec22473c42af6d31504d48c4
sankaranagabalaji <sankaranagabalaji@gmail.com>
2025-03-21 11:21:50 +0530

    Ternary operator

--- Test.java
+++ Test.java
@@ -2,12 +2,9 @@ import java.util.Scanner;
 public class Test{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("x=");
-        int x = scanner.nextInt();
-        System.out.print("y=");
-        int y = scanner.nextInt();
-        System.out.print("z=");
-        int z = scanner.nextInt();
+        System.out.println("Income: ");
+        int income=scanner.nextInt();
+        String className = income>10_000 ? "First_Class":"Economy";
+        System.out.println(className);
     }
 }
