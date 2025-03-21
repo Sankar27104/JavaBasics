6d4d13f90de2b19e758514cc979b9bcc9b3047a6
sankaranagabalaji <sankaranagabalaji@gmail.com>
2025-03-21 18:34:53 +0530

    Testing while loop

--- Test.java
+++ Test.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
+
 public class Test{
     public static void main(String[] args){
+        String input = "";
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Income: ");
-        int income=scanner.nextInt();
-        String className = income>10_000 ? "First_Class":"Economy";
-        System.out.println(className);
+        while(!input.equals("quit")){
+            System.out.print("Input:");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+            System.out.println("Enter \"quit\" to exit");
+        }
     }
 }
