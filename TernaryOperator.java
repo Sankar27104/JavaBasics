15ffdd34c4ea09d93018d31140680b15bd087edc
sankaranagabalaji <sankaranagabalaji@gmail.com>
2025-05-17 11:36:10 +0530

    Using Ternary Operator

--- TernaryOperator.java
+++ TernaryOperator.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class TernaryOperator {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Income:");
-        int income=scanner.nextInt();
 
-        String className = income>100000 ? "First Class" : "Economy";
-        System.out.println(className);
+        System.out.print("Enter the number: ");
+        int number = scanner.nextInt();
+
+        String oddOrEven = number%2==0 ? "Even" : "Odd";
+        System.out.println(oddOrEven);
     }
 }
