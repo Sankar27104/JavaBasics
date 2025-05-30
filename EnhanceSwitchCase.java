bd53fdf74bb728c00d9a81fdb5a44dce8e1e172a
sankaranagabalaji <sankaranagabalaji@gmail.com>
2025-05-30 20:15:29 +0530

    Enhance switch cases|Prefer to use

--- EnhanceSwitchCase.java
+++ EnhanceSwitchCase.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class EnhanceSwitchCase {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the day: ");
+        String day = scanner.nextLine().toUpperCase();
+        System.out.println(day);
+
+        switch (day){
+            case "MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY" -> System.out.println(day + " is a weekday");
+            case "SATURDAY","SUNDAY" -> System.out.println(day + " is a weekend");
+            default -> System.out.println(day + " is not a day");
+        }
+        scanner.close();
+    }
 }
